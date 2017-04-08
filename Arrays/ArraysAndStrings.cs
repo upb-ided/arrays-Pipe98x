f75f792437e597d586702923868a51410a8817ed
Pipe98x <pipe989898@gmail.com>
2017-04-07 23:32:04 -0500

    Todos los puntos resueltos

--- Arrays/ArraysAndStrings.cs
+++ Arrays/ArraysAndStrings.cs
@@ -24,18 +24,38 @@
         {
             int result = 1;
 
-            while(currentNumber < result)
+            while (currentNumber >= result)
             {
                 result = result * 2;
             }
 
             return result;
+
         }
 
         public int FibonacciAtN(int fibonacciPositioon)
         {
             int result = 0;
+            int primerIndice = 0;
+            int segundoIndice = 0;
+            int numero = 0;
 
+            for (int i = 0; i < fibonacciPositioon; i++)
+            {
+                if (i == 0)
+                {
+                    numero = 0;
+                    segundoIndice += 1;
+                }
+                else
+                {
+                    numero = primerIndice + segundoIndice;
+                    primerIndice = segundoIndice;
+                    segundoIndice = numero;
+                }
+            }
+
+            result = numero;
             return result;
         }
 
@@ -59,7 +79,7 @@
         {
             bool result = false;
 
-            if ( inputNum % inputFactor == 0)
+            if (inputNum % inputFactor == 0)
             {
                 result = true;
             }
@@ -70,8 +90,8 @@
         {
             bool result = true;
 
-            
-                
+
+
 
             return result;
         }
@@ -80,7 +100,7 @@
         {
             bool result = false;
 
-          
+
             return result;
         }
 
@@ -90,7 +110,7 @@
 
             if (inputArr.Length > 1)
             {
-                for (int i = 0; i < inputArr.Length; i++)
+                for (int i = 0; i < inputArr.Length; i += 2)
                 {
                     result += inputArr[i];
                 }
@@ -110,11 +130,15 @@
 
             for (int i = 0; i < inputString.Length; i++)
             {
-                if ((inputString[i] == 'a') || (inputString[i] == 'e') || (inputString[i] == 'i') || (inputString[i] == 'o') || (inputString[i] == 'u'))
+                if (inputString[i].GetType() == typeof(char))
                 {
-                    result += 1;
-                   }
+                    if ((inputString[i] == 'a') || (inputString[i] == 'e') || (inputString[i] == 'i') || (inputString[i] == 'o') || (inputString[i] == 'u'))
+                    {
+                        result += 1;
+                    }
                 }
+            }
+
             return result;
         }
 
@@ -140,10 +164,12 @@
             if ((inputString.Length % 2) == 0)
             {
                 result = "PAR";
-            }else {
+            }
+            else
+            {
                 result = "IMPAR";
             }
-            
+
 
             //TODO: Para indicar que el conteo es par, retornar "PAR"
             //Para indicar que el conteo es inpar, retornar "IMPAR"
@@ -164,7 +190,8 @@
             if ((producto % 2) == 0)
             {
                 result = "PAR";
-            }else
+            }
+            else
             {
                 result = "IMPAR";
             }
@@ -177,21 +204,21 @@
         public string RacerGotFirst(float d1, float t1, float d2, float t2)
         {
             string result = string.Empty;
-			float v1 = 0.0f;
-			float v2 = 0.0f;
+            float v1 = 0.0f;
+            float v2 = 0.0f;
 
-			v1 = (d1 / t1);
-			v2 = (d2 / t2);
+            v1 = (d1 / t1);
+            v2 = (d2 / t2);
 
-			if (v1 > v2)
-			{
-				result = "PRIMERO";
-			}
-			else
-			{
-				result = "SEGUNDO";
-			}
-	
+            if (v1 > v2)
+            {
+                result = "PRIMERO";
+            }
+            else
+            {
+                result = "SEGUNDO";
+            }
+
             //TODO: Para indicar que el corredor más rápido es el primero, retornar "PRIMERO"
             //Para indicar que el corredor más rápido es el segundo, retornar "SEGUNDO"
 
